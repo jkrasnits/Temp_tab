c798fb6169cab940d10f1881382514fcc496b168
Jkrasnits <jkrasnits@gmail.com>
2014-07-03 20:10:28 -0400

    added context menu

--- background.js
+++ background.js
@@ -1,2 +1,18 @@
 var tabs = [];
 
+chrome.contextMenus.create({
+	"id": "temp_tab",
+	"title": "Add to temp_tab",
+	"contexts": ["link"],
+	"onclick": contextMenuClickHandler
+	}, function(){
+		console.log("context menu item created");
+	}
+);
+
+function contextMenuClickHandler(info, tab){
+	chrome.tabs.executeScript(null, {file: "inject.js"}, function(data){
+		//var track = JSON.parse(data[0]);
+		//tracks.push(track);
+	});
+}
---
+++ inject.js
@@ -0,0 +1,8 @@
+var tab = {};
+
+var selected_link = document.activeElement;
+
+tab.link = selected_link.getAttribute("href");
+tab.name = selected_link.innerHTML;
+
+console.log(tab);
