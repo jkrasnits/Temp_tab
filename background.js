1dc858e813fd7be34bc0abc9f7a49e6892a1e118
jkrasnits <jkrasnits@gmail.com>
2014-07-04 23:18:01 -0400

    added minor comments

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 var tabs = [];
 
+
+//creates context menu with add option only when a right click is done on a link
 chrome.contextMenus.create({
 	"id": "temp_tab",
 	"title": "Add to temp_tab",
@@ -10,6 +12,7 @@ chrome.contextMenus.create({
 	}
 );
 
+//handler for the add option - runs inject.js and then adds resulting object to array of temp_tabs
 function contextMenuClickHandler(info, tab){
 	chrome.tabs.executeScript(null, {file: "inject.js"}, function(tab){
 		tabs.push(tab);
--- inject.js
+++ inject.js
@@ -1,10 +1,11 @@
+//tab object will initially contain the href attr and the link innerHTML when it is sent to the server
 var tab = {};
 
 var selected_link = document.activeElement;
 
 tab.link = selected_link.getAttribute("href");
-tab.name = selected_link.innerHTML;
+tab.title = selected_link.innerHTML;
+
 
 console.log(tab);
-
 JSON.stringify(tab);
