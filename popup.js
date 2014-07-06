4500702e9ec300baeeda5c2d76d709575eabf02a
Jkrasnits <jkrasnits@gmail.com>
2014-07-06 15:53:49 -0400

    started popup.js

--- popup.js
+++ popup.js
@@ -1,2 +1,18 @@
-//this will have js for the tab storing window
+//retrieves the tabs array from the background page
+var background = chrome.extension.getBackgroundPage();
+var tabs = background.tabs;
 
+
+
+$( document ).ready(function() {
+	populate_popup();
+});
+
+//loops through tabs array and populates popup
+function populate_popup () {
+	for (var i = 0; i < tabs.length; i++) {
+		var current_tab = JSON.parse(tabs[i][0]);
+		$("#container").append("<p>", current_tab.title, " : ", current_tab.link, "</p>");
+	};
+
+}
