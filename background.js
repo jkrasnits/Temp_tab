d0cb41b45b7d5b3cd53010e316acbf318887ab62
Jkrasnits <jkrasnits@gmail.com>
2014-07-07 21:36:27 -0400

    made context menu appear only for reddit

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
 var tabs = [];
 
+chrome.tabs.getCurrent(function(tab){
+        console.log(tab.url);
+    }
+);
 
 //creates context menu with add option only when a right click is done on a link
 chrome.contextMenus.create({
 	"id": "temp_tab",
 	"title": "Add to temp_tab",
+	"documentUrlPatterns":["http://www.reddit.com/*"],
 	"contexts": ["link"],
 	"onclick": contextMenuClickHandler
 	}, function(){
